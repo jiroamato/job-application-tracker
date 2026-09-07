afdd923f8479c62cbc32afc6282d57efe12cceba
Jiro Amato <amato.miguel@gmail.com>
2026-09-07 15:53:56 -0700

    feat: add caching to getBoard function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,18 +3,14 @@ import { getSession } from "@/lib/auth/auth";
 import connectDB from "@/lib/db";
 import { Board } from "@/lib/models";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
-export default async function Dashboard() {
-  const session = await getSession();
-
-  if (!session?.user) {
-    redirect("/sign-in");
-  }
-
+async function getBoard(userId: string) {
+  "use cache";
   await connectDB();
 
-  const board = await Board.findOne({
-    userId: session.user.id,
+  const boardDoc = await Board.findOne({
+    userId: userId,
     name: "Job Hunt",
   }).populate({
     path: "columns",
@@ -22,6 +18,21 @@ export default async function Dashboard() {
       path: "jobApplications",
     },
   });
+
+  if (!boardDoc) return null;
+
+  const board = JSON.parse(JSON.stringify(boardDoc));
+
+  return board;
+}
+
+async function DashboardPage() {
+  const session = await getSession();
+  const board = await getBoard(session?.user.id ?? "");
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
 
   console.log(board);
 
@@ -32,11 +43,16 @@ export default async function Dashboard() {
           <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
           <p className="text-gray-600">Track your job applications</p>
         </div>
-        <KanbanBoard
-          board={JSON.parse(JSON.stringify(board))}
-          userId={session.user.id}
-        />
+        <KanbanBoard board={board} userId={session.user.id} />
       </div>
     </div>
+  );
+}
+
+export default async function Dashboard() {
+  return (
+    <Suspense fallback={<p>Loading ...</p>}>
+      <DashboardPage />
+    </Suspense>
   );
 }
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -15,6 +15,12 @@ export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
   }),
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
